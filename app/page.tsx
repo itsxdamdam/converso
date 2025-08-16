031412b9a3d2af26c35e15bbd04f14efb647d0aa
Bakare Samuel Oluwadamilola <49197462+itsxdamdam@users.noreply.github.com>
2025-08-16 23:02:50 +0100

    complete table and cta section

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,48 @@
-import { Button } from "@/components/ui/button";
+import CompanionCard from "@/components/CompanionCard";
+import CompanionList from "@/components/CompanionList";
+import CTA from "@/components/CTA";
+import { recentSessions } from "@/constants";
 import React from "react";
 
 const Page = () => {
   return (
-    <div>
-      <h1 className="text-2xl underline">Welcome to my Converso</h1>
-      <Button>Let's get Started</Button>
-    </div>
+    <main>
+      <h1 className="text-2xl underline">Popular Companions</h1>
+      <section className="home-section">
+        <CompanionCard
+          id="123"
+          name="Neura the Brainy Explorer"
+          topic="Neural Network of the Brain"
+          subject="science"
+          duration={45}
+          color="#ffda6e"
+        />
+        <CompanionCard
+          id="456"
+          name="Countsy the Number Wizard"
+          topic="Neural Network of the Brain"
+          subject="Derivatives & Integrals"
+          duration={30}
+          color="#e5d0ff"
+        />
+        <CompanionCard
+          id="789"
+          name="Verba the Vocabulary Builder"
+          topic="language"
+          subject="English Literature"
+          duration={30}
+          color="#bde7ff"
+        />
+      </section>
+      <section className="home-section">
+        <CompanionList
+          title="Recently completed sessions"
+          companions={recentSessions}
+          classNames="w-3/3 max-lg:w-full"
+        />
+        <CTA />
+      </section>
+    </main>
   );
 };
 
