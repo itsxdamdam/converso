97beb9056044ea7b199f78251624b92f865ae91a
Bakare Samuel Oluwadamilola <49197462+itsxdamdam@users.noreply.github.com>
2025-08-18 13:32:11 +0100

    fix: sign portal button bg

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${bricolage.variable} antialiased`}>
-        <ClerkProvider appearance={{ variables: { colorPrimary: "#fe5p33" } }}>
+        <ClerkProvider appearance={{ variables: { colorPrimary: "#fe5933" } }}>
           <Navbar />
           {children}
         </ClerkProvider>
