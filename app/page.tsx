01074d6c2c789ea88887394901616b050098e3a8
Bakare Samuel Oluwadamilola <49197462+itsxdamdam@users.noreply.github.com>
2025-08-21 01:27:10 +0100

    modifications on main

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import HomeSection from "@/components/HomeSection";
 
 const Page = async () => {
   return (
-    <main>
+    <main className="pt-0 gap-4">
       <h1>Popular Companions</h1>
       <HomeSection />
     </main>
